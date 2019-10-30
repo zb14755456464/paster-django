43fa7bec41b07e1fb7dafbe4029c9d978bb81851
root <root@kubia.example.com>
2019-10-30 00:46:00 -0400

    fix migrations

--- lico/job/test_app/migrations/0001_lico_5_4_0.py
+++ lico/job/test_app/migrations/0001_lico_5_4_0.py
@@ -13,9 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Human',
             fields=[
-                ('id', models.AutoField(
-                    auto_created=True, primary_key=True,
-                    serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('age', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
             ],
